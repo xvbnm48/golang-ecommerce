e5a50c51828ef0bfa50955e96e9db7f473ade5ff
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-05-23 19:08:42 +0700

    cart.go, controller.go

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,9 +1,67 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"time"
 
-func AddToCart() gin.Handler {
+	"github.com/gin-gonic/gin"
+	"github.com/xvbnm48/golang-ecommerce/database"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
+type Application struct {
+	prodColletion  *mongo.Collection
+	userCollection *mongo.Collection
+}
+
+func NewApplication(prodColletion, userCollection *mongo.Collection) *Application {
+	return &Application{
+		prodColletion:  prodColletion,
+		userCollection: userCollection,
+	}
+}
+
+func (app *Application) AddToCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("Product ID is empty")
+
+			_ = c.AbortWithError(http.StatusInternalServerError, errors.New("Product ID is empty"))
+			return
+		}
+
+		userQueryID := c.Query("user_id")
+		if userQueryID == "" {
+			log.Println("User ID is empty")
+
+			_ = c.AbortWithError(http.StatusInternalServerError, errors.New("User ID is empty"))
+			return
+		}
+
+		productID, err := primitive.ObjectIDFromHex(productQueryID)
+
+		if err != nil {
+			log.Println("Product ID is invalid")
+
+			_ = c.AbortWithError(http.StatusInternalServerError, errors.New("Product ID is invalid"))
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.AddProductToCart(ctx, app.prodColletion, app.userCollection, productID, userQueryID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+
+		c.IndentedJSON(200, "successfully added to cart")
+	}
 }
 
 func RemoteItem() gin.HandlerFunc {
