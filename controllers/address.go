3f810d49b4848fa7591f22c3d5f85f40c6badf9a
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-19 22:54:08 +0700

    controllers address: try for complete function add address

--- controllers/address.go
+++ controllers/address.go
@@ -9,10 +9,52 @@ import (
 	"github.com/xvbnm48/golang-ecommerce/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func AddAdress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "invalid code",
+			})
+			c.Abort()
+			return
+		}
 
+		address, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{
+				"error": "internal server error",
+			})
+
+			return
+		}
+
+		var addresses models.Address
+		addresses.Address_id = primitive.NewObjectID()
+
+		if err := c.BindJSON(&addresses); err != nil {
+			c.IndentedJSON(http.StatusNotAcceptable, err.Error())
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		match_filter := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: address}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$address"}}}}
+		group := bson.D{{Key: "$group", Value: bson.D{primitive.E{
+			Key:   "_id",
+			Value: "$address_id",
+		}, {Key: "count", Value: bson.D{primitive.E{Key: "$sum", Value: 1}}}}}}
+		pointCursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{match_filter, unwind, group})
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "internal server error")
+			return
+		}
+
+	}
 }
 
 func EditHomeAdress() gin.HandlerFunc {
